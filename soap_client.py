3cf45a86c694884fdb25fa1a2cbb89d9606a258b
pm4113 <pm4113@mci4me.at>
2017-07-26 18:18:05 +0200

    Weitere Änderungen bei der Abfrage des Soap Servers

--- soap_client.py
+++ soap_client.py
@@ -4,38 +4,55 @@ import datetime
 import xml.etree.ElementTree as ET
 from refresh_files import *
 from open_files import *
+from identify_data import *
+from write_database import *
 
-debug = True
+
+debug = False
 url = "http://194.208.52.114:8080/axis2/services/DataCollection?wsdl"
 headers = {'Content-type':'text/html'}
 replacement = 'Project_EWA_' + unicode(datetime.datetime.now())
-get_data = True
+get_response = True
 
 refresh_Arlberghaus()
-Kunde1 = open_Arlberghaus()
+Kunde1_request = open_Arlberghaus()
 if debug:
-	print Kunde1[0]
-	print Kunde1[1]
-	print Kunde1[2]
-	print Kunde1[3]
+	print Kunde1_request[0]
+	print Kunde1_request[1]
+	print Kunde1_request[2]
+	print Kunde1_request[3]
                                    
 while(True):
-	ARL_Strom = requests.post(url, data=Kunde1[0], headers=headers)
-	ARL_Wasser = requests.post(url, data=Kunde1[1], headers=headers)
-	if debug:
-		print ARL_Strom.content
-		print ARL_Wasser.content
-	while(get_data):                   
-	       	time.sleep(10)
-      		ARL_Strom_poll = requests.post(url, data=Kunde1[2], headers=headers)
-     		ARL_Wasser_poll = requests.post(url, data=Kunde1[3], headers=headers)
-		if debug:
-    			print ARL_Strom_poll.content 
-			print ARL_Wasser_poll.content
+#	get_response = True
+#	Kunde1_strom = requests.post(url, data=Kunde1_request[0], headers=headers) #Strom
+#	Kunde1_wasser = requests.post(url, data=Kunde1_request[1], headers=headers) #Wasser
+#	if debug:
+#		print Kunde1_strom.content
+#		print Kunde1_wasser.content
+#	while(get_response):                   
+			time.sleep(10)
+ #    		Kunde1_strom_poll = requests.post(url, data=Kunde1_request[2], headers=headers)
+  #   		Kunde1_wasser_poll = requests.post(url, data=Kunde1_request[3], headers=headers)
+#		if debug:
+ #   			print Kunde1_strom_poll.content 
+#			print Kunde1_wasser_poll.content
 		
-		with open("Arlberghaus_Response_Strom.xml", "wb") as d:
-			d.write(ARL_Strom_poll.text)
-			d.close()
-		with open("Arlberghaus_Response_Wasser.xml", "wb") as f:
-			f.write(ARL_Wasser_poll.text)
-			f.close()
+#		with open("Arlberghaus_Response_Strom.xml", "wb") as d:
+#			d.write(Kunde1_strom_poll.text)
+#			d.close()
+			Kunde1_response_strom = get_data_kunde1_strom()
+			print "Aktueller Stromstand: " + str(Kunde1_response_strom)
+
+#		with open("Arlberghaus_Response_Wasser.xml", "wb") as f:
+#			f.write(Kunde1_wasser_poll.text)
+#			f.close()
+			Kunde1_response_wasser = get_data_kunde1_wasser()
+			print "Aktueller Wasserstand: " + str(Kunde1_response_wasser)
+		
+			if  Kunde1_response_wasser > 0:
+				if Kunde1_response_strom > 0:
+					get_response = False
+					writeIntoDB(Kunde1_response_strom, Kunde1_response_wasser)
+		
+
+
